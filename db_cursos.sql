9047d1ed63062d78a0ae6a5e1ceece15804dea09
Raul Perez <raulperod@gmail.com>
2017-12-18 12:37:45 -0700

    script sql v2

--- db_cursos.sql
+++ db_cursos.sql
@@ -24,3 +24,114 @@ CREATE TABLE `usuarios` (
   UNIQUE KEY `correo` (`correo`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cursos` (
+  `idCurso` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `division` varchar(50) NULL,
+  `departamento` varchar(50) NULL,
+  `caracter` varchar(80) NULL,
+  `objetivoGeneral` TEXT NULL,
+  `objetivosEspecificos` TEXT NULL,
+  `contenidoSintetico` TEXT NULL,
+  `formaEnsenanza` TEXT NULL,
+  `requisitosDeEvaluacion` TEXT NULL,
+  `bibliografia` TEXT NULL,
+  `perfilAcademico` TEXT NULL,
+  `capacidadAutoFin` TEXT NULL,
+  `utilidad` TEXT NULL,
+  `expInstructor` TEXT NULL,
+  `AntecedentesAlumnos` TEXT NULL,
+  `duracion` TEXT NULL,
+  `cupoMaximo` int(10) unsigned NULL,
+  `cupoMinimo` int(10) unsigned NULL,
+  `numeroDeParticipantes` int(10) unsigned NULL,
+  `reqIdioma` varchar(70) NULL,
+  `infraestructuraNecesaria` TEXT NULL,
+  `cargoInstructor` TEXT NULL,
+  `dependencia` varchar(50) NULL,
+  `telefono` varchar(20) NULL,
+  `fechaInicio` date NULL,
+  `fechaFinal` date NULL,
+  PRIMARY KEY (`idCurso`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `cursos_usuarios` (
+  `idUsuario` int(10) unsigned NOT NULL,
+  `idCurso` int(10) unsigned NOT NULL,
+  `tipo` int(10) unsigned NOT NULL,
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cursos_usuarios_asistencia` (
+  `idUsuario` int(10) unsigned NOT NULL,
+  `idCurso` int(10) unsigned NOT NULL,
+  `fecha` date NULL,
+  `asistio` tinyint(1) NOT NULL DEFAULT '0',
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cursos_usuarios_evaluacion_curso` (
+  `idUsuario` int(10) unsigned NOT NULL,
+  `idCurso` int(10) unsigned NOT NULL,
+  `expectativas` int(3) unsigned NOT NULL,
+  `pertinencia` int(3) unsigned NOT NULL,
+  `topicos` int(3) unsigned NOT NULL,
+  `tiempos` int(3) unsigned NOT NULL,
+  `objetivos` int(3) unsigned NOT NULL,
+  `materiales` int(3) unsigned NOT NULL,
+  `aplicacion` int(3) unsigned NOT NULL,
+  `medios` int(3) unsigned NOT NULL,
+  `informacion` int(3) unsigned NOT NULL,
+  `programa` int(3) unsigned NOT NULL,
+  `sugerencias` TEXT NULL,
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cursos_usuarios_evaluacion_instructor` (
+  `idUsuario` int(10) unsigned NOT NULL,
+  `idCurso` int(10) unsigned NOT NULL,
+  `presentacionDeConceptos` int(3) unsigned NOT NULL,
+  `interaccionYmotivacion` int(3) unsigned NOT NULL,
+  `usoDeRecursosDidacticos` int(3) unsigned NOT NULL,
+  `comunicacionConElGrupo` int(3) unsigned NOT NULL,
+  `laExtensionDeLaInformacion` int(3) unsigned NOT NULL,
+  `estrategias` int(3) unsigned NOT NULL,
+  `desempeno` int(3) unsigned NOT NULL,
+  `sugerencias` TEXT NULL,
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cursos_usuarios_evaluacion_participantes` (
+  `idUsuario` int(10) unsigned NOT NULL,
+  `idCurso` int(10) unsigned NOT NULL,
+  `aprobo` tinyint(1) NOT NULL DEFAULT '0',
+  `calificado` tinyint(1) NOT NULL DEFAULT '0',
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idCurso` (`idCurso`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `cursos_usuarios`
+ADD CONSTRAINT `cursos_usuarios_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `cursos_usuarios_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`idCurso`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `cursos_usuarios_asistencia`
+ADD CONSTRAINT `cursos_usuarios_asistencia_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `cursos_usuarios_asistencia_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`idCurso`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `cursos_usuarios_evaluacion_curso`
+ADD CONSTRAINT `cursos_usuarios_evaluacion_curso_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `cursos_usuarios_evaluacion_curso_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`idCurso`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `cursos_usuarios_evaluacion_instructor`
+ADD CONSTRAINT `cursos_usuarios_evaluacion_instructor_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `cursos_usuarios_evaluacion_instructor_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`idCurso`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `cursos_usuarios_evaluacion_participantes`
+ADD CONSTRAINT `cursos_usuarios_evaluacion_participantes_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `cursos_usuarios_evaluacion_participantes_ibfk_2` FOREIGN KEY (`idCurso`) REFERENCES `cursos` (`idCurso`) ON DELETE CASCADE ON UPDATE NO ACTION;
