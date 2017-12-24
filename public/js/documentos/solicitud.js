92b4f9909202a616d278c2d88bb3369fd976b0b9
Raul Perez <raulperod@gmail.com>
2017-12-23 23:36:33 -0700

    documento: registro

--- public/js/documentos/solicitud.js
+++ public/js/documentos/solicitud.js
@@ -102,7 +102,21 @@ function generarSolicitudCurso(nombreRepresentante,nombreCurso) {
 }
 
 function generarRegistro(curso, correo) {
-    var docDefinition = {
+      //obtener en formato bien la fecha inicial y la fecha final del curso
+      var fechaI=curso.fechaInicio;
+      var fechaF=curso.fechaFinal;
+      var elem = fechaI.split('-');
+      anoI = elem[0];
+      mesI = obtenerMes(elem[1]);
+      aux=elem[2].split('T');
+      diaI = aux[0];
+      var elem = fechaF.split('-');
+      anoF = elem[0];
+      mesF = obtenerMes(elem[1]);
+      aux=elem[2].split('T');
+      diaF = aux[0];
+      //Aqui ya tengo las fechas
+      var docDefinition = {
         content: [
             {
                 style: 'tableExample',
@@ -124,14 +138,14 @@ function generarRegistro(curso, correo) {
                         [{text: 'Requisitos de evaluación:',style: [ 'campo']}, curso.requisitosDeEvaluacion],
                         [{text: 'Bibliografía:',style: [ 'campo']}, curso.bibliografia],
                         [{text: 'Perfil académico deseable del responsable de la materia:',style: [ 'campo']}, curso.perfilAcademico],
-                        [{text: 'Capacidad de autofinanciamiento:',style: [ 'campo']}, curso.capacidadAutofin],
+                        [{text: 'Capacidad de autofinanciamiento:',style: [ 'campo']}, curso.capacidadAutoFin],
                         [{text: 'Utilidad y oportunidad en funcion del programa:',style: [ 'campo']}, curso.utilidad],
                         [{text: 'Experiencia, calidad profesional y academica del instructor:',style: [ 'campo']}, 'Se adjunta'],
                         [{text: 'Antecedentes o habilidades necesarias de los alumnos:',style: [ 'campo']}, curso.antecedentesAlumnos],
                         [{text: 'Duración del programa:',style: [ 'campo']}, curso.duracion],
                         [{text: 'Cupo mínimo y máximo:',style: [ 'campo']}, 'de '+curso.cupoMinimo+' a '+curso.cupoMaximo+' participantes'],
                         [{text: 'Requisitos de idioma:',style: [ 'campo']}, curso.reqIdioma],
-                        [{text: 'Fecha de inicio y terminación:',style: [ 'campo']}, 'Iniciara el '+curso.fechaInicio+' y terminara el '+curso.fechaFinal],
+                        [{text: 'Fecha de inicio y terminación:',style: [ 'campo']}, 'Iniciará el '+diaI+' de '+mesI+ ' del '+anoI+' y terminará el '+diaF+' de '+mesF+ ' del '+anoF],
                         [{text: 'Infraestructura necesaria para ofrecer el curso:',style: [ 'campo']}, curso.infraestructuraNecesaria],
                         [{text: 'Cargo del instructor:',style: [ 'campo']}, curso.cargoInstructor],
                         [{text: 'Dependecia:',style: [ 'campo']}, curso.dependencia],
@@ -169,4 +183,46 @@ function generarRegistro(curso, correo) {
     };
     pdfMake.createPdf(docDefinition).open();
     //pdfMake.createPdf(docDefinition).download('Constancia.pdf');
+}
+//funcion para obtener en letra el mes obtenido
+function obtenerMes(mes){
+      switch(mes) {
+            case '1':
+                  mes='Enero';
+                  break;
+            case '2':
+                  mes='Febrero';
+                  break;
+            case '3':
+                  mes='Marzo';
+                  break;
+            case '4':
+                  mes='Abril';
+                  break;
+            case '5':
+                  mes='Mayo';
+                  break;
+            case '6':
+                  mes='Junio';
+                  break;
+            case '7':
+                  mes='Julio';
+                  break;
+            case '8':
+                  mes='Agosto';
+                  break;
+            case '9':
+                  mes='Septiembre';
+                  break;
+            case '10':
+                  mes='Octubre';
+                  break;
+            case 11:
+                  mes='Noviembre';
+                  break;
+            case '12':
+                  mes='Diciembre';
+                  break;
+      }
+      return mes;
 }
